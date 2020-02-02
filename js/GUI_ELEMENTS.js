ecb510e0234ae3a9dccaed256a873a2053bbb307
David <d.chkhaidze@gmail.com>
2020-02-02 09:13:57 -0800

    .

--- js/GUI_ELEMENTS.js
+++ js/GUI_ELEMENTS.js
@@ -35,13 +35,13 @@ function isGUI_Click(is_Page,is_DataRole,is_ID,is_Value1,is_Value2){
 			e.stopImmediatePropagation(); \
 			e.preventDefault(); \
 			console.log("'+is_ID+' : Out_'+is_Value1+' = '+is_Value2+'"); \
-			});});'	
+			});});</script>'	
 	);
 }
 
 //##################################
 function isGUI_Slider(is_DataRole,is_ID,is_Min, is_Max, is_Step) {
-$('[data-role="'+is_DataRole+'"]').append('<input name="'+is_ID+'" id="'+is_ID+'" type="range"  min="'+is_Min+'" max="'+is_Max+'" value="'+is_Min+'" />');
+$('[data-role="'+is_DataRole+'"]').append('<input name="'+is_ID+'" id="'+is_ID+'" type="range"  min="'+is_Min+'" max="'+is_Max+'" step="'+is_Step+'"  value="'+is_Min+'" />');
 }
 
 function isGUI_Slider_Stop(is_Page,is_DataRole,is_ID,is_OutPut){
@@ -50,28 +50,38 @@ function isGUI_Slider_Stop(is_Page,is_DataRole,is_ID,is_OutPut){
 		$("#'+is_ID+'").on( \'slidestop\', function( event ) \
 		{ var Value = $("#'+is_ID+'").val(); \
 		console.log("'+is_ID+' : Out_'+is_OutPut+' = " + Value ); \
-		});});'
+		});});</script>'
 	);
 }
 
-
-
+//##################################
+function isGUI_flip(is_DataRole,is_ID,is_Name1,is_Val1,is_Name2,is_Val2){
+	$('[data-role="'+is_DataRole+'"]').append('<select name="'+is_ID+'" id="'+is_ID+'" data-role="slider"><option value="'+is_Val2+'">'+is_Name2+'</option><option value="'+is_Val1+'">'+is_Name1+'</option></select>');
+}	
+function isGUI_flip_Change(is_Page,is_DataRole,is_ID,is_OutPut){
+	$('[data-role="'+is_DataRole+'"]').append('<script> \
+		$(document).on("pageinit","#'+is_Page+'",function(){ \
+		$("#'+is_ID+'").on( \'change\', function( event ) \
+		{ var Value = $("#'+is_ID+'").val(); \
+		console.log("'+is_ID+' : Out_'+is_OutPut+' = " + Value ); \
+		});});</script>'
+	);
+}
 //##################################
 function isGUI_Text(is_DataRole,is_Value){
 	$('[data-role="'+is_DataRole+'"]').append('<label><br>'+is_Value+'</label>'); 
 }
-
-
-function isGUI_flip(is_DataRole,is_ID,is_Val1, is_Val2){
-	$('[data-role="'+is_DataRole+'"]').append('<select name="'+is_ID+'" id="'+is_ID+'" data-role="slider"><option value="on">'+is_Val2+'</option><option value="off">'+is_Val1+'</option></select>');
-}	
-function isGUI_flip_Change(is_Page,is_DataRole,is_ID,is_Value){
+//##################################
+function isGUI_Select_Change(is_Page,is_DataRole,is_ID,is_OutPut){
 	$('[data-role="'+is_DataRole+'"]').append('<script> \
 		$(document).on("pageinit","#'+is_Page+'",function(){ \
-		$("#'+is_ID+'").on( \'change\', function( event ) \
-		{ var Value = $("#'+is_ID+'").val();'+is_Value+'});});'
-	);
+		$("#'+is_ID+'").change(function(){\
+		var Value = $("#'+is_ID+'").val();\
+		console.log("'+is_ID+' : Out_'+is_OutPut+' = " + Value ); \
+		});});</script>'
+	);	
 }
+//##################################
  
 
 
