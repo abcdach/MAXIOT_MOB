212091dc0cab13aeea656a225fa206494997f8f6
David <d.chkhaidze@gmail.com>
2020-02-03 03:46:10 -0800

    .

--- js/GUI_ELEMENTS.js
+++ js/GUI_ELEMENTS.js
@@ -11,11 +11,11 @@ function isGUI_Input(is_DataRole,is_ID,is_Type,is_Value) {
 }
 
 
-function isGUI_Input_Text(is_DataRole,is_ID,is_Value) {
-	$('[data-role="'+is_DataRole+'"]').append('<input id="'+is_ID+'" type="text" value="'+is_Value+'"/>'); 	
+function isGUI_Input_Text(is_ID,is_Value) {
+	return('<input id="'+is_ID+'" type="text" value="'+is_Value+'"/>'); 	
 }
-function isGUI_Input_Password(is_DataRole,is_ID,is_Value) {
-	$('[data-role="'+is_DataRole+'"]').append('<input id="'+is_ID+'" type="password" value="'+is_Value+'"/>'); 	
+function isGUI_Input_Password(is_ID,is_Value) {
+	return('<input id="'+is_ID+'" type="password" value="'+is_Value+'"/>'); 	
 }
 //##################################
 
@@ -24,8 +24,8 @@ function isGUI_Input_Value(is_ID,is_Value){$('#'+is_ID).val(is_Value)}
 
 
 
-function isGUI_Button(is_DataRole,is_ID,is_Value) {
-	$('[data-role="'+is_DataRole+'"]').append('<input id="'+is_ID+'" type="button" value="'+is_Value+'"/>'); 	
+function isGUI_Button(is_ID,is_Value) {
+	return('<input id="'+is_ID+'" type="button" value="'+is_Value+'"/>'); 	
 }
 function isGUI_Click(is_Page,is_DataRole,is_ID,is_Value1,is_Value2){
 	console.log(is_Value1);	
@@ -40,8 +40,8 @@ function isGUI_Click(is_Page,is_DataRole,is_ID,is_Value1,is_Value2){
 }
 
 //##################################
-function isGUI_Slider(is_DataRole,is_ID,is_Min, is_Max, is_Step) {
-$('[data-role="'+is_DataRole+'"]').append('<input name="'+is_ID+'" id="'+is_ID+'" type="range"  min="'+is_Min+'" max="'+is_Max+'" step="'+is_Step+'"  value="'+is_Min+'" />');
+function isGUI_Slider(is_ID,is_Min, is_Max, is_Step) {
+	return('<input name="'+is_ID+'" id="'+is_ID+'" type="range"  min="'+is_Min+'" max="'+is_Max+'" step="'+is_Step+'"  value="'+is_Min+'" />');
 }
 
 function isGUI_Slider_Stop(is_Page,is_DataRole,is_ID,is_OutPut){
@@ -55,8 +55,8 @@ function isGUI_Slider_Stop(is_Page,is_DataRole,is_ID,is_OutPut){
 }
 
 //##################################
-function isGUI_flip(is_DataRole,is_ID,is_Name1,is_Val1,is_Name2,is_Val2){
-	$('[data-role="'+is_DataRole+'"]').append('<select name="'+is_ID+'" id="'+is_ID+'" data-role="slider"><option value="'+is_Val2+'">'+is_Name2+'</option><option value="'+is_Val1+'">'+is_Name1+'</option></select>');
+function isGUI_flip(is_ID,is_Name1,is_Val1,is_Name2,is_Val2){
+	return('<select name="'+is_ID+'" id="'+is_ID+'" data-role="slider"><option value="'+is_Val2+'">'+is_Name2+'</option><option value="'+is_Val1+'">'+is_Name1+'</option></select>');
 }	
 function isGUI_flip_Change(is_Page,is_DataRole,is_ID,is_OutPut){
 	$('[data-role="'+is_DataRole+'"]').append('<script> \
@@ -68,8 +68,9 @@ function isGUI_flip_Change(is_Page,is_DataRole,is_ID,is_OutPut){
 	);
 }
 //##################################
-function isGUI_Text(is_DataRole,is_Value){
-	$('[data-role="'+is_DataRole+'"]').append('<label><br>'+is_Value+'</label>'); 
+
+function isGUI_Text(is_Value){
+	return('<label><br>'+is_Value+'</label>'); 
 }
 //##################################
 function isGUI_Select_Change(is_Page,is_DataRole,is_ID,is_OutPut){
@@ -83,6 +84,16 @@ function isGUI_Select_Change(is_Page,is_DataRole,is_ID,is_OutPut){
 }
 //##################################
  
+
+
+
+
+
+
+
+
+
+
 
 
 
