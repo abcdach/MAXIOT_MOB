6285375c537cdb6468e0ebd1c5f0f519aaaee304
David <d.chkhaidze@gmail.com>
2020-02-04 12:41:06 -0800

    .

--- js/GUI_ELEMENTS.js
+++ js/GUI_ELEMENTS.js
@@ -27,17 +27,7 @@ function isGUI_Input_Value(is_ID,is_Value){$('#'+is_ID).val(is_Value)}
 function isGUI_Button(is_ID,is_Value) {
 	return('<input id="'+is_ID+'" type="button" value="'+is_Value+'"/>');
 }
-function xxxisGUI_Click(is_Page,is_DataRole,is_ID,is_Value1,is_Value2){
-	console.log(is_Value1);	
-	$('[data-role="'+is_DataRole+'"]').append('<script> \
-		$(document).on("pageinit","#'+is_Page+'",function(){ \
-		$("#'+is_ID+'").click(function (e){ \
-			e.stopImmediatePropagation(); \
-			e.preventDefault(); \
-			console.log("'+is_ID+' : Out_'+is_Value1+' = '+is_Value2+'"); \
-			});});</script>'
-	);
-}
+
 function isGUI_Click(is_Page,is_DataRole,is_ID,is_Script){
 	$('[data-role="'+is_DataRole+'"]').append('<script> \
 		$(document).on("pageinit","#'+is_Page+'",function(){ \
